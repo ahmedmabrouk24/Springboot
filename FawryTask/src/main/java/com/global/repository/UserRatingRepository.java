b97a0e4ab7df11a4544aa11540d5e757ab68ba60
Ahmed mabrouk fekry abdelfattah <58913522+ahmedmabrouk24@users.noreply.github.com>
2025-03-16 03:04:46 +0200

    Delete FawryTask directory

--- FawryTask/src/main/java/com/global/repository/UserRatingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.global.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.global.entity.UserRating;
-
-public interface UserRatingRepository extends JpaRepository<UserRating, Long> {
-    // Find a Rating by userId and movieId
-    UserRating findByUser_IdAndMovie_ImdbID(Long userId, String movieImdbID);
-}
